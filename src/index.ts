a99f4792f9d9a566ee593db0abd7a0c77e99508e
Tushar-Genefied <tushar.saini@genuinemark.org>
2023-04-18 17:02:10 +0530

    eslint ts setup

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+const app = require("express")();
+
+app.listen(4000,()=>{
+	console.log("run ");
+});
+const favoriteFruits: string[] = ["apple", "strawberry", "orange"];
+
+function addFruit(fruit : any) {
+	favoriteFruits.push(fruit);
+	// const name = "tushar";
+	// const hi = "yo";
+	let num : number;
+	console.log(num);
+	const array : [] = [];
+	console.log(array);
+	console.log(num);
+	console.error("hi");
+	console.log("2");
+
+  
+}
+
+addFruit("string");
+
+
